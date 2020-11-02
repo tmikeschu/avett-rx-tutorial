eab8a9a58cfa7aaddd129066f7be3e3cb9749c4e
Mike Schutte <tmikeschutte@gmail.com>
2020-11-01 21:00:13 -0500

    set up eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+import * as React from "react";
+import { AppProps } from "next/dist/next-server/lib/router/router";
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
 
 export default MyApp;
