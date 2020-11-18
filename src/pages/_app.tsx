9a23f8aadf168cdc6eee4a2f1d3947eb0f965eca
Mike Schutte <tmikeschutte@gmail.com>
2020-11-17 22:25:27 -0500

    add custom chakra theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,22 @@
 import "../styles/globals.css";
 
 import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 
 import { ApolloProvider } from "lib/apollo-client";
+import theme from "styles/theme";
 
 const AvettRxApp: React.FC<AppProps> = ({
   Component,
   pageProps: { initialApolloState, ...pageProps },
 }) => {
   return (
-    <ApolloProvider initialState={initialApolloState || {}}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <ChakraProvider theme={theme}>
+      <ApolloProvider initialState={initialApolloState || {}}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
   );
 };
 
