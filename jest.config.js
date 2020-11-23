09a485fb557adf79995604f344375a888734121f
Mike Schutte <tmikeschutte@gmail.com>
2020-11-23 10:46:24 -0500

    add jest deps and config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  transform: {
+    "^.+\\.tsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^(components|features|lib|test)/(.*)$": "<rootDir>/src/$1/$2",
+    "^(test)$": "<rootDir>/src/$1",
+    "^(api)$": "<rootDir>/__generated__/$1",
+    "\\.(css)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup-env.ts"],
+};
---
+++ src/test/setup-env.ts
@@ -0,0 +1,17 @@
+beforeEach(() => {
+  console.log("before each");
+});
+
+beforeAll(() => {
+  console.log("before all");
+});
+
+afterEach(() => {
+  console.log("after each");
+});
+
+afterAll(() => {
+  console.log("after all");
+});
+
+export {};
