39d1f826a93f2b3efeff120c364884867791b6b1
Mike Schutte <tmikeschutte@gmail.com>
2020-12-07 09:38:02 -0500

    add router provider to storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../src/styles/theme";
 import { worker } from "../src/mocks/browser";
 import { ApolloProvider } from "../src/test/apollo-provider";
+import RouterProvider from "../src/test/router-provider";
 
 worker.start();
 
@@ -11,9 +12,13 @@ export const parameters = {
 };
 
 export const decorators = [
-  (story) => (
-    <ChakraProvider theme={theme}>
-      <ApolloProvider>{story()}</ApolloProvider>
-    </ChakraProvider>
+  (Story) => (
+    <RouterProvider>
+      <ChakraProvider theme={theme}>
+        <ApolloProvider>
+          <Story />
+        </ApolloProvider>
+      </ChakraProvider>
+    </RouterProvider>
   ),
 ];
