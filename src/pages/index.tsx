c922f7636e5f6a1fcfa17f2f6c1dca7fe1515ea3
Mike Schutte <tmikeschutte@gmail.com>
2020-12-07 22:34:21 -0500

    unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
 import Head from "next/head";
 import NextLink from "next/link";
