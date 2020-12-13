735c908de8b402f67237c713945e7246c0f3cbc1
Mike Schutte <tmikeschutte@gmail.com>
2020-12-12 23:35:51 -0500

    next app type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 
 import * as React from "react";
-import { AppProps } from "next/dist/next-server/lib/router/router";
+import { AppProps } from "next/app";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
