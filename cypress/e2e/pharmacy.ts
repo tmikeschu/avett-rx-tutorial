06eb2e077ffbab5e3a17aea6036bdf5270703eba
Mike Schutte <tmikeschutte@gmail.com>
2020-12-13 10:31:18 -0500

    better cypress

--- cypress/e2e/pharmacy.ts
+++ cypress/e2e/pharmacy.ts
@@ -2,7 +2,7 @@ describe("Pharmacy", () => {
   it("can be visited from homescreen button", () => {
     cy.viewport("iphone-6").visit("/");
     cy.findByText(/get a prescription/i).click();
-    cy.location("pathname").should("match", /^\/pharmacy$/);
+    cy.location("pathname", { timeout: 5000 }).should("match", /^\/pharmacy$/);
     cy.findByRole("heading", { name: /pharmacy/i }).should("exist");
     cy.findByText(/select a feeling/i).should("exist");
     cy.findByRole("button", { name: /back/i }).click();
